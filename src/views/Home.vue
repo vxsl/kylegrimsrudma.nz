cfcfe6d51ef490d95f2a07aef64cc0c8f649ac54
Kyle Grimsrud-Manz <kyle.grimsrudmanz@gmail.com>
2021-01-15 11:30:04 -0500

    mobile fixes

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,5 @@
 <template>
-
-  <div id="fullpage">
+  <div id="fullpage" >
     <div class="section">
       <div :class="'inner-section ' + mobileClass">
         <Nav :mobile="mobile" :mobileClass="mobileClass"/>
@@ -17,9 +16,7 @@
               </div>
             </div>
           </div>
-        </div>
-        <div id="jumbo" ref="jumbo" :class="mobile? 'mobile' : 'col-5'" class="jumbotron bg-primary text-light" align="right">
-          <div class="container">
+          <div id="jumbo" ref="jumbo" :class="mobileClass" class="jumbotron bg-primary text-light" align="right">
             <div id="tplysource" ref="tplysource">
               <type v-pre data-type="type1">Hello workl,.</type>
               <wait v-pre>500ms</wait>
@@ -31,16 +28,22 @@
               <type v-pre data-type="type1">Hello world.</type>
             </div>
             <h1 id="tplydestination" ref="tplydestination" class="display-4 text-light"></h1>
-            <p id="primary-sub" class="text-light">My name is Kyle Grimsrud-Manz.</p>
-            <p id="secondary-sub" class="text-light">I'm in my final year of a <span id="bold">Bachelor of Computer Science</span> at Concordia University in Montreal, and I love programming.<br></p>
-            <b-button id="contact-button" variant="dark" class="btn-lg" href="mailto:hi@kylegrimsrudma.nz">Feel free to reach out.</b-button>
+            <div class="container">
+              <p id="primary-sub" class="text-light">My name is Kyle Grimsrud-Manz.</p>
+              <p id="secondary-sub" class="text-light">I'm in my final year of a <span id="bold">Bachelor of Computer Science</span> at Concordia University in Montreal, and I love programming.<br></p>
+              <b-button id="contact-button" variant="dark" class="btn-lg" href="mailto:hi@kylegrimsrudma.nz">Feel free to reach out.</b-button>
+            </div>
+          </div>
+          <div v-if="mobile" id="mobile-blurb">
+              <div class="text-dark">
+                Thanks for visiting my website! 👨‍💻<br><br>Take a look at the <a href="https://markt.kylegrimsrudma.nz">markt</a> webapp or the <a href="https://github.com/vxsl/recoverability">recoverability</a> desktop application.
+              </div>
           </div>
         </div>
-        <div class="col-2"></div>
-      </b-row>
+      </div>
     </div>
-    <div class="section">
-      <b-row class="d-flex justify-content-center">
+    <div v-if="!mobile" class="section">
+      <b-row class="w-100 d-flex justify-content-center">
         <b-container fluid id="blurb">
           <b-col>
             <div class="alert align-middle alert-primary bg-light" role="alert">
@@ -51,7 +54,7 @@
         </b-container>
       </b-row>
     </div>
-    <div class="section summary-section">
+    <div v-if="!mobile" class="section summary-section">
       <div class="d-flex justify-content-center">
         <div class="calendar text-light"></div>
       </div>
@@ -86,91 +89,88 @@ export default {
     }
   },
   mounted() {
+    if (!this.mobile) {
       var fullPageInstance = new fullpage('#fullpage', {
-        navigation: true,
-        paddingTop: '3em',
-        //sectionsColor:['#ff5f45', '#0798ec', '#fc6c7c', 'grey']
       });
-      this.$nextTick(function(){
-
-
-        tply.animate(
-          this.$refs.tplysource,
-          this.$refs.tplydestination,
-          {
-            "types": [
-              {
-                "name": "type1",
-                "properties": {
-                    "data-char-interval": "50ms",
-                    "data-word-interval": "700ms"
-                }
-              },
-              {
-                "name": "type2",
-                "properties": {
-                    "data-char-interval": "200ms",
-                    "data-word-interval": "700ms"
-                }
+      GitHubCalendar(".calendar", "vxsl", {responsive: true});
+    }
+    this.$nextTick(function(){
+      tply.animate(
+        this.$refs.tplysource,
+        this.$refs.tplydestination,
+        {
+          "types": [
+            {
+              "name": "type1",
+              "properties": {
+                  "data-char-interval": "50ms",
+                  "data-word-interval": "700ms"
               }
-            ]
-        });
+            },
+            {
+              "name": "type2",
+              "properties": {
+                  "data-char-interval": "200ms",
+                  "data-word-interval": "700ms"
+              }
+            }
+          ]
+      });
+    })
 
-        GitHubCalendar(".calendar", "vxsl", {responsive: true});
+/* 
+    var prevScrollpos = window.pageYOffset;
+    var jumbo = this.$refs.jumbo;
+    var images = this.$refs.images;
+    var row = this.$refs.greeting;
+    var hidden = false;
+    var unhideFlag = false;
 
-        var prevScrollpos = window.pageYOffset;
-        var jumbo = this.$refs.jumbo;
-        var images = this.$refs.images;
-        var row = this.$refs.greeting;
-        var hidden = false;
-        var unhideFlag = false;
+    window.addEventListener("scroll", function(){
+      console.log("scroll")
+      //var jumboClasses = jumbo.classList;
+      var currentScrollPos = window.pageYOffset;
+      console.log(window.scrollY)
+      //console.log(hidden)
+      if (prevScrollpos == 0 && !hidden) {
+        hidden = true
+        console.log("here")
+        images.style.left = "140%";
+        images.style.top = '-100vh';
+        jumbo.style.top = "-100vh";
+        jumbo.classList.remove("quick")
+        images.classList.remove("quick")
+        row.style.height = "0";
+        setTimeout(() => {
+          unhideFlag = true;
+        }, 1000);
+      }
 
-        window.scroll(() => {
-          console.log("HERE")
-        })
-
-        window.addEventListener("scroll", function(){
-          console.log("scroll")
-          //var jumboClasses = jumbo.classList;
-          var currentScrollPos = window.pageYOffset;
-          console.log(window.scrollY)
-          //console.log(hidden)
-          if (prevScrollpos == 0 && !hidden) {
-            hidden = true
-            console.log("here")
-            images.style.left = "140%";
-            images.style.top = '-100vh';
-            jumbo.style.top = "-100vh";
-            jumbo.classList.remove("quick")
-            images.classList.remove("quick")
-            row.style.height = "0";
-            setTimeout(() => {
-              unhideFlag = true;
-            }, 1000);
-          }
-
-          if (currentScrollPos == 0 && hidden && unhideFlag) {
-            hidden = false
-            row.style.height = "auto";
-            jumbo.classList.add("quick")
-            images.classList.add("quick")
-            jumbo.style.top = "0";
-            images.style.top = '0';
-            images.style.left = "0";
-            
-          }
-          prevScrollpos = currentScrollPos;
-        })
-      })
-    },
+      if (currentScrollPos == 0 && hidden && unhideFlag) {
+        hidden = false
+        row.style.height = "auto";
+        jumbo.classList.add("quick")
+        images.classList.add("quick")
+        jumbo.style.top = "0";
+        images.style.top = '0';
+        images.style.left = "0";
+        
+      }
+      prevScrollpos = currentScrollPos;
+    }) */
+  },
 }
 </script>
-
 <style lang="scss">
 @import '../../scss/custom.scss';
-@import '../../scss/fullpage-custom.css';
+//@import '../../scss/fullpage-custom.css';
+@import '../../node_modules/fullpage.js/dist/fullpage.min.css';
 @import '../../scss/GitHubCalendar-custom.scss';
 
+.fp-tableCell {
+  display: flex;
+  align-items: center;
+  padding:15%;
 }
 Nav {
   margin-bottom:1em;
@@ -183,7 +183,8 @@ Nav {
   }
 }
 
-.summary-section {
+
+.summary-section {  
   padding-left:20%;
   padding-right:20%;
   .calendar {
@@ -242,12 +243,14 @@ Nav {
   //transition: left 0.3s !important;
 }
 #greeting {
-
+  height:100%;
   display:flex;
+  &.mobile {
+    display:inline-block
+  }
   transition: height 0.3s;
   //transition-delay: 1s;
   transition-timing-function: linear;
-
   * {
     user-select:none;
   }
@@ -262,7 +265,7 @@ Nav {
     -ms-user-select: none; /* IE10+/Edge */
     user-select: none; /* Standard */
     //white-space: nowrap;
-    height:4em;
+    height:3em;
   }
 
   @keyframes blink {
@@ -287,29 +290,27 @@ Nav {
     margin-left: 0.2em;
   }
 
-  .col-3 {
-    padding:0;
-    overflow:visible !important;
-  }
-
   #bold {
     font-weight:500;
   }
 
   #images {
+    width:50%;
     position:relative;
-    height:100%;
+    min-height:100%;
     transition-property: left, top;
     transition-duration: 0.4s, 0.3s;
     transition-delay: 0s, 0.4s;
     transition-timing-function: linear;
 
     #img-container {
+
       float:right;
       height:100%;
+      width:100%;
       display:flex;
       #codeimg {
-        width:100%;
+        max-height:100%;
         position:relative;
         object-fit:cover;
         border-top-right-radius:0;
@@ -318,12 +319,13 @@ Nav {
         border-bottom-left-radius:3em;
       }
       #portrait-container {
-        display:flex;
-        float:right;
+        width:100%;
         height:100%;
+        //display:flex;
+        float:right;
         #portraitimg, #portrait-overlay {
-          width:100%;
           position:absolute;
+          height:100%;
           object-fit:cover;
           border-top-right-radius:0;
           border-bottom-right-radius:0;
@@ -331,13 +333,13 @@ Nav {
           border-bottom-left-radius:3em;
         }
         #portraitimg {
-          height:100%;
           left:25%;
           z-index:0;
         }
         #portrait-overlay {
           opacity:0;
           left:25%;
+          width:100%;
           background-color:theme-color("light");
           color:theme-color("primary");
           height:100%;
@@ -358,14 +360,49 @@ Nav {
     }
   }
 
-  #jumbo {
-    &.mobile {
+  #mobile-blurb {
+    z-index:3;
+    border:solid;
+    border-color:theme-color('dark');
+    border-width:1px;
+    border-radius:1em;
+    margin-top:1em;
+    font-size:1.2em;
+    width:100%;
+    padding:1em;
+    a {
+      font-weight:700;
     }
+  }
+  #jumbo {
+    min-height:100%;
+    &.mobile {
+      min-width:100vw;
+      border-radius:1em;
+      width:100vw;
+      #tplydestination {
+        z-index:2;
+        height:2em;
+      }
+      .container {
+        width:100%;
+        bottom:0;
+      }
+      #primary-sub {
+        font-size:1.7em;
+        font-weight:400;
+      }
+      #secondary-sub {
+        max-width:100%;
+      }
+    }
+    
     z-index:2;
-    padding:3em !important;
-    display:flex;
+    //padding:3em !important;
     border-radius:2em;
-    top: 0; /* Stay on top */
+    border-top-right-radius:1em;
+    border-bottom-right-radius:1em;
+    border-top-left-radius:7em;
     max-height:100%;
     margin-bottom:0px !important;
     transition: top 1s;
@@ -413,4 +450,5 @@ Nav {
     font-size:1.2em;
   }
 }
+
 </style>
